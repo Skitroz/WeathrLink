5cc78c76da1dedea2b3187d034d6a0211810906a
Skitroz <65279654+Skitroz@users.noreply.github.com>
2023-08-18 19:16:48 +0200

    Update main.php

--- app/src/accueil/main.php
+++ app/src/accueil/main.php
@@ -11,7 +11,24 @@
 </head>
 <body class="font">
     <main>
-        <h1 class="text-center pt-10 text-xl">Bienvenue sur <span id="titre" class="underline text-[#3b82f6] font-bold"></span></h1>
+        <h1 class="text-center sm:ml-[300px] ml-10 pt-10 text-2xl">Bienvenue sur <span id="titre" class="underline text-[#3b82f6] font-bold"></span></h1>
+        <div class="flex flex-col lg:flex-row sm:mx-32 sm:ml-[400px] mx-8 ml-16 text-center pt-10 gap-8">
+            <div class="shadow-[5px_5px_5px_5px_rgba(0,0,0,0.2)] px-4 sm:px-8">
+                <i class="fa-solid fa-bullseye text-3xl my-4"></i>
+                <h2 class="mb-1 text-xl">Précision inégalée</h2>
+                <p class="mb-4">Bénéficiez de prévisions météo d'une précision exceptionnelle grâce à nos technologies de pointe. Planifiez en toute confiance vos trajets et activités en plein air.</p>
+            </div>
+            <div class="shadow-[5px_5px_5px_5px_rgba(0,0,0,0.2)] px-4 sm:px-8">
+                <i class="fa-solid fa-handshake-simple text-3xl my-4"></i>
+                <h2 class="mb-1 text-xl">Simplicité d'utilisation</h2>
+                <p class="mb-4">Avec notre interface conviviale, accédez rapidement aux infos météo essentielles. Plus de recherches, tout en un seul endroit.</p>
+            </div>
+            <div class="shadow-[5px_5px_5px_5px_rgba(0,0,0,0.2)] px-4 sm:px-8 ">
+                <i class="fa-solid fa-thumbs-up text-3xl my-4"></i>
+                <h2 class="mb-1 text-xl">Personnalisé à votre mode de vie</h2>
+                <p class="mb-4">WeathrLink s'ajuste à vos besoins : sports extérieurs, voyages ou confort quotidien, des prévisions météo personnalisées pour vous.</p>
+            </div>
+        </div>
     </main>
 </body>
 </html>
