606d44e467a8c2da7ca4c8f2a9d91e945674974a
Skitroz <65279654+Skitroz@users.noreply.github.com>
2023-08-18 22:22:32 +0200

    Update

--- app/src/accueil/header.php
+++ app/src/accueil/header.php
@@ -14,14 +14,14 @@
 <body class="font">
     <header>
         <menu>
-            <nav class="bg-blue-500 absolute left-0 text-gray-100 h-screen flex flex-col items-center justify-between py-4 w-12 sm:w-auto">
-                    <a href="/"><img src="public/img/logo.png" alt="Logo WeathrLink" class="w-[80px] sm:w-[100px] mb-32 opacity-[0.65]"></a>
-                    <ul class="flex flex-col mb-40 text-xl gap-2">
+            <nav class="bg-blue-500 fixed left-0 text-gray-100 h-screen flex flex-col items-center justify-between py-4 w-12 sm:w-auto">
+                <a href="/"><img src="public/img/logo.png" alt="Logo WeathrLink" class="w-[80px] sm:w-[100px] mb-32 opacity-[0.65]"></a>
+                <ul class="flex flex-col mb-40 text-xl gap-2">
                     <li><a href="#" class="flex items-center gap-2 sm:hover:px-6 sm:hover:bg-blue-600 sm:hover:py-2 sm:hover:rounded mx-12 sm:mx-0 sm:px-10 h-10 sm:w-[300px]"><i class="fa-solid fa-house"></i><span class="hidden sm:block">Accueil</span></a></li>
                     <li class="relative group">
                         <button id="meteo" class="flex items-center gap-2 sm:hover:px-6 sm:hover:bg-blue-600 sm:hover:py-2 sm:hover:rounded mx-12 sm:mx-0 sm:px-10 h-10 sm:w-[300px]"><i id="icon-meteo" class="fa-solid fa-sun h-5"></i><span class="mr-28 hidden sm:block">Météo</span></button>
                         <ul id="sousMenu" class="opacity-0 invisible space-y-2 transition transition-all ease-in-out h-0 items-center flex flex-col">
-                            <li><a href="#" class="flex gap-2 items-center sm:hover:bg-blue-600 sm:px-8 py-2 mx-1 sm:mx-12 hover:rounded "><i class="fa-solid fa-mobile-screen-button"></i><span class="hidden sm:block">iPhone</span></a></li>
+                            <li><a href="app/src/meteoIphone/meteoIphone.php" class="flex gap-2 items-center sm:hover:bg-blue-600 sm:px-8 py-2 mx-1 sm:mx-12 hover:rounded "><i class="fa-solid fa-mobile-screen-button"></i><span class="hidden sm:block">iPhone</span></a></li>
                             <li><a href="#" class="flex gap-2 items-center sm:hover:bg-blue-600 sm:px-8 py-2 mx-1 sm:mx-12 hover:rounded"><i class="fa-solid fa-cloud"></i><span class="hidden sm:block">France</span></a></li>
                         </ul>
                     </li>
--- app/src/accueil/main.php
+++ app/src/accueil/main.php
@@ -12,21 +12,30 @@
 <body class="font">
     <main>
         <h1 class="text-center sm:ml-[300px] ml-10 pt-10 text-2xl">Bienvenue sur <span id="titre" class="underline text-[#3b82f6] font-bold"></span></h1>
-        <div class="flex flex-col lg:flex-row sm:mx-32 sm:ml-[400px] mx-8 ml-16 text-center pt-10 gap-8">
-            <div class="shadow-[5px_5px_5px_5px_rgba(0,0,0,0.2)] px-4 sm:px-8">
+        <div class="flex flex-col lg:flex-row sm:mx-32 sm:ml-[400px] mx-8 ml-16 text-center pt-10 gap-8 mb-16">
+            <div class="shadow-[5px_5px_5px_5px_rgba(0,0,0,0.2)] px-4 sm:px-8 w-[280px] md:w-[500px] h-[250px]">
                 <i class="fa-solid fa-bullseye text-3xl my-4"></i>
                 <h2 class="mb-1 text-xl">Précision inégalée</h2>
                 <p class="mb-4">Bénéficiez de prévisions météo d'une précision exceptionnelle grâce à nos technologies de pointe. Planifiez en toute confiance vos trajets et activités en plein air.</p>
             </div>
-            <div class="shadow-[5px_5px_5px_5px_rgba(0,0,0,0.2)] px-4 sm:px-8">
+            <div class="shadow-[5px_5px_5px_5px_rgba(0,0,0,0.2)] px-4 sm:px-8 w-[280px] md:w-[500px] h-[250px]">
                 <i class="fa-solid fa-handshake-simple text-3xl my-4"></i>
                 <h2 class="mb-1 text-xl">Simplicité d'utilisation</h2>
                 <p class="mb-4">Avec notre interface conviviale, accédez rapidement aux infos météo essentielles. Plus de recherches, tout en un seul endroit.</p>
             </div>
-            <div class="shadow-[5px_5px_5px_5px_rgba(0,0,0,0.2)] px-4 sm:px-8 ">
+            <div class="shadow-[5px_5px_5px_5px_rgba(0,0,0,0.2)] px-4 sm:px-8 md:w-[500px] w-[280px] h-[250px]">
                 <i class="fa-solid fa-thumbs-up text-3xl my-4"></i>
                 <h2 class="mb-1 text-xl">Personnalisé à votre mode de vie</h2>
                 <p class="mb-4">WeathrLink s'ajuste à vos besoins : sports extérieurs, voyages ou confort quotidien, des prévisions météo personnalisées pour vous.</p>
+            </div>
+        </div>
+        <h2 class="text-center sm:ml-[300px] ml-10 pt-10 text-2xl mb-4">Quelle météo souhaites-tu consulter ?</h2>
+        <div class="text-center flex justify-center items-center mb-4">
+            <div class="ml-12 sm:ml-[300px]">
+                <a href="#"><button class="shadow py-2 px-3 rounded bg-[#2859a9] text-white hover:bg-[#15305c] transition ease-in-out delay-150">Météo iPhone</button></a>
+            </div>
+            <div class="ml-12">
+                <a href="#"><button class="shadow py-2 px-3 rounded bg-[#2859a9] text-white hover:bg-[#15305c] transition ease-in-out delay-150">Météo France</button></a>
             </div>
         </div>
     </main>
